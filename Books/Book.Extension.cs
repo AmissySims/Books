a54be028b788aefad80e091d01c6d3ba37b1bb44
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-01-22 21:01:37 +0300

    Добавьте файлы проекта.

---
+++ Books/Book.Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public partial class Book
+    {
+        public string ColorDis
+        {
+            get
+            {
+                if (CurrentDiscount > 15)
+                    return "#7fff00";
+                else
+                    return "#ffffff";
+            }
+        }
+    }
+}
